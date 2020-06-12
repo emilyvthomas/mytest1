c2ba8142f0deceda20f3067184ac2ee5f307490e
emilyvthomas <42291317+emilyvthomas@users.noreply.github.com>
2020-06-12 12:49:07 +0530

    connection.cs
    Implemented connection file

---
+++ connection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+/// <summary>
+/// Summary description for connection
+/// </summary>
+public class connection
+{
+    SqlConnection cn;
+    SqlCommand cm;
+    SqlDataReader dr;
+    public connection()
+    {
+        cn = new SqlConnection();
+        string ss;
+        ss = AppDomain.CurrentDomain.BaseDirectory;
+        cn.ConnectionString = "Data Source=.\\SQLEXPRESS;AttachDbFilename=" + ss + "productdata.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+        cn.Open();
+    }
+    public void insert(String s)
+    {
+
+        cn.Close();
+        cn.Open();
+        cm = new SqlCommand(s, cn);
+        cm.ExecuteNonQuery();
+
+    }
+    public SqlDataReader disp(String s)
+    {
+        cn.Close();
+        cn.Open();
+        cm = new SqlCommand(s, cn);
+        dr = cm.ExecuteReader();
+        return (dr);
+    }
+}
